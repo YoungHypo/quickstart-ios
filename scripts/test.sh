b59ad7b6b69f4b2f949a53ae625bbacba8fddbf9
haibo <haibo942us@gmail.com>
2025-09-01 20:55:44 -0700

    fix ci

--- scripts/test.sh
+++ scripts/test.sh
@@ -20,10 +20,7 @@
 
 set -euo pipefail
 
-if [ -d "/Applications/Xcode_26_beta_6.app" ]; then
-    xcode_version="26_beta_6"
-    iphone_version="16"
-elif [ -d "/Applications/Xcode_16.2.app" ]; then
+if [ -d "/Applications/Xcode_16.2.app" ]; then
     xcode_version="16.2"
     iphone_version="16"
 else
